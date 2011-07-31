028f5de0fc00b777a7cb8d06c8c3e6ce7ea306ee
Øyvind Bakkeli <olivaq@gmail.com>
2011-07-31 22:23:28 +0200

    flyttet meta

--- loop.php
+++ loop.php
@@ -6,15 +6,16 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class( $post_class_stuff ); ?>>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<div class="loop-image">
-				<?php echo get_the_post_thumbnail( $post->ID, array (525, 9999) ); ?>
-			</div>
-
 			<div class="entry-meta">
 				<?php twentyten_posted_on(); ?>
 			</div><!-- .entry-meta -->
 
-			<div class="entry-content">
+
+			<div class="loop-image">
+				<?php echo get_the_post_thumbnail( $post->ID, array (525, 9999) ); ?>
+			</div>
+
+						<div class="entry-content">
 
 						<?php the_excerpt(); ?>
 
